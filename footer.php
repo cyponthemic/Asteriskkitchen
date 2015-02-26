b07e22641ed3c31ca89bb2abc94afb5ef1d5b404
Alex <Chavet>
2015-02-27 09:56:53 +1100

    Style V1

--- footer.php
+++ footer.php
@@ -1,9 +1,27 @@
-</section>
+</section>	
+<!--
 <footer class="row">
 	<?php do_action('foundationPress_before_footer'); ?>
 	<?php dynamic_sidebar("footer-widgets"); ?>
 	<?php do_action('foundationPress_after_footer'); ?>
+	
 </footer>
+-->
+	<footer class="row social-bar">
+			<div id="social" class="large-7 medium-8 columns opacity-white">
+				<div class="inner">
+				<a href="https://twitter.com/asteriskkitchen">
+					<i class="fi-social-twitter">Test</i>
+				</a> 
+			
+				<a href="http://www.facebook.com/AsteriskKitchen?fref=ts" >
+					<i class="fi-social-twitter">Test</i>
+				</a> 
+				</		
+			</div>
+	</footer><!-- #colophon -->
+
+
 <a class="exit-off-canvas"></a>
 
 	<?php do_action('foundationPress_layout_end'); ?>
--- parts/top-bar.php
+++ parts/top-bar.php
@@ -1,13 +1,15 @@
-<div class="row">
-    <nav class="top-bar large-7 medium-9 columns" data-topbar role="navigation">
-        <ul class="title-area">
-            <li class="name">
-                <h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
-            </li>
-        </ul>
-        <section class="top-bar-section">
+<div class="row show-for-medium-up">
+	 <div class="large-7 medium-8 columns opacity-white center-elements	">
+               <img class="header-logo" src="<?php header_image(); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" />
+     </div>
+</div>
+<div class="row show-for-medium-up">
+    <nav class="top-bar large-7 medium-8 columns" data-topbar role="navigation">
+            
+        <section class="top-bar-section center-elements">
             <?php foundationPress_top_bar_l(); ?>
             <?php foundationPress_top_bar_r(); ?>
         </section>
+       
     </nav>
 </div>
